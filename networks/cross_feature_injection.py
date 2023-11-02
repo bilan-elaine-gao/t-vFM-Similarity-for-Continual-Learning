20009f3eff95721a3fbada01b567ae267c8ad3fe
bilan-elaine-gao <80089158+bilan-elaine-gao@users.noreply.github.com>
2023-11-02 17:14:53 +0900

    Add files via upload

---
+++ networks/cross_feature_injection.py
@@ -0,0 +1,125 @@
+import itertools
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#exmaple_1 code for CDN
+class Crosslayer_1(nn.Module):
+    """The Cross Network part of Deep&Cross Network model,
+    which leans both low and high degree cross feature.
+      Input shape
+        - 2D tensor with shape: ``(batch_size, units)``.
+      Output shape
+        - 2D tensor with shape: ``(batch_size, units)``.
+      Arguments
+        - **in_features** : Positive integer, dimensionality of input features.
+        - **input_feature_num**: Positive integer, shape(Input tensor)[-1]
+        - **layer_num**: Positive integer, the cross layer number
+        - **parameterization**: string, ``"vector"``  or ``"matrix"`` ,  way to parameterize the cross network.
+        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix
+        - **seed**: A Python integer to use as random seed.
+      References
+        - [Wang R, Fu B, Fu G, et al. Deep & cross network for ad click predictions[C]//Proceedings of the ADKDD'17. ACM, 2017: 12.](https://arxiv.org/abs/1708.05123)
+        - [Wang R, Shivanna R, Cheng D Z, et al. DCN-M: Improved Deep & Cross Network for Feature Cross Learning in Web-scale Learning to Rank Systems[J]. 2020.](https://arxiv.org/abs/2008.13535)
+    """
+
+    def __init__(self, in_features, layer_num=2, parameterization='vector', seed=1024): #,device='cpu'
+        super(Crosslayer_1, self).__init__()
+        self.layer_num = layer_num
+        self.parameterization = parameterization
+        if self.parameterization == 'vector':
+            # weight in DCN.  (in_features, 1)
+            self.kernels = nn.Parameter(torch.Tensor(self.layer_num, in_features, 1))
+        elif self.parameterization == 'matrix':
+            # weight matrix in DCN-M.  (in_features, in_features)
+            self.kernels = nn.Parameter(torch.Tensor(self.layer_num, in_features, in_features))
+        else:  # error
+            raise ValueError("parameterization should be 'vector' or 'matrix'")
+
+        self.bias = nn.Parameter(torch.Tensor(self.layer_num, in_features, 1))
+
+        for i in range(self.kernels.shape[0]):
+            nn.init.xavier_normal_(self.kernels[i])
+        for i in range(self.bias.shape[0]):
+            nn.init.zeros_(self.bias[i])
+
+        #self.to(device)
+
+    def forward(self, inputs): #input:[512, 64, 32, 32]
+        x_0 = inputs.unsqueeze(2) #unsqueeze(2)  [512, 64, 1,32, 32]
+       
+        x_l = x_0
+        for i in range(self.layer_num):
+            if self.parameterization == 'vector':
+                xl_w = torch.tensordot(x_l, self.kernels[i],dims=([1], [0])) #dims=([1], [0])  [256, 1, 32, 32, 1]
+                
+                dot_ = torch.matmul(x_0, xl_w)  #[256, 64, 32, 32, 1]
+                
+                x_l = dot_  + x_l  #x_l = dot_ + self.bias[i] + x_l
+                
+            elif self.parameterization == 'matrix':
+                xl_w = torch.matmul(self.kernels[i], x_l)  # W * xi  (bs, in_features, 1)
+                dot_ = xl_w + self.bias[i]  # W * xi + b
+                x_l = x_0 * dot_ + x_l  # x0 · (W * xi + b) +xl  Hadamard-product
+            else:  # error
+                raise ValueError("parameterization should be 'vector' or 'matrix'")
+        x_l = torch.squeeze(x_l, dim=2)
+        return x_l
+
+
+
+#exmaple_2 code for CDN
+#code from https://github.com/xue-pai/FuxiCTR/blob/963c4ea57d5c766e05ed5f1f41b1ed8084855183/fuxictr/pytorch/layers/interactions/cross_net.py 
+class CrossInteraction(nn.Module):
+    def __init__(self, input_dim):
+        super(CrossInteraction, self).__init__()
+        self.weight = nn.Linear(input_dim, 1, bias=False)
+        self.bias = nn.Parameter(torch.zeros(input_dim))
+
+    def forward(self, X_0, X_i):
+        interact_out = self.weight(X_i) * X_0 + self.bias
+        return interact_out
+
+
+class Crosslayer_2(nn.Module):
+    def __init__(self, input_dim, num_layers):
+        super(Crosslayer_2, self).__init__()
+        self.num_layers = num_layers
+        self.cross_net = nn.ModuleList(CrossInteraction(input_dim)
+                                       for _ in range(self.num_layers))
+
+    def forward(self, X_0):
+        X_i = X_0 # b x dim
+        for i in range(self.num_layers):
+            X_i = X_i + self.cross_net[i](X_0, X_i)
+        return X_i
+
+
+#FM layer
+class FM(nn.Module):
+    """Factorization Machine models pairwise (order-2) feature interactions
+     without linear term and bias.
+      Input shape
+        - 3D tensor with shape: ``(batch_size,field_size,embedding_size)``.
+      Output shape
+        - 2D tensor with shape: ``(batch_size, 1)``.
+      References
+        - [Factorization Machines](https://www.csie.ntu.edu.tw/~b97053/paper/Rendle2010FM.pdf)
+    """
+
+    def __init__(self,inputs):
+        super(FM, self).__init__()
+        self.inputs=inputs
+
+    def forward(self, inputs):
+        fm_input = inputs 
+
+        square_of_sum = torch.pow(torch.sum(fm_input, dim=1, keepdim=True), 2)
+       
+        sum_of_square = torch.sum(fm_input * fm_input, dim=1, keepdim=True)
+        
+        cross_term = square_of_sum - sum_of_square
+        cross_term = 0.5 * torch.sum(cross_term, dim=2, keepdim=False)
+
+        return cross_term
